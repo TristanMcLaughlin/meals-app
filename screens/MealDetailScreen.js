832e603c8a30282e11070ba734e188f0c8accbcc
Becca <becca.mclaughlin@laser.red>
2020-02-29 13:32:37 +0000

    Add header icon

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,39 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/CustomHeaderButton";
 
 const MealDetailScreen = props => {
-    const mealId = props.navigation.getParam('mealId');
-    const meal = MEALS.find(meal => meal.id === mealId);
+  const mealId = props.navigation.getParam("mealId");
+  const meal = MEALS.find(meal => meal.id === mealId);
 
-    return (
-        <View style={styles.screen}>
-            <Text>
-                {meal.title}
-            </Text>
-            <Button title="Categories" onPress={() => {
-                // Go back to topmost level
-                props.navigation.popToTop()
-            }} />
-        </View>
+  return (
+    <View style={styles.screen}>
+      <Text>{meal.title}</Text>
+      <Button
+        title="Categories"
+        onPress={() => {
+          // Go back to topmost level
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+MealDetailScreen.navigationOptions = route => {
+  const mealId = route.navigation.getParam("mealId");
+  const meal = MEALS.find(meal => meal.id === mealId);
+
+  return {
+    title: meal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Fave"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("mark as favourite");
+          }}
+        />
+      </HeaderButtons>
     )
-}
-
-MealDetailScreen.navigationOptions = (route) => {
-    const mealId = route.navigation.getParam('mealId');
-    const meal = MEALS.find(meal => meal.id === mealId);
-
-    return {
-        title: meal.title
-    }
-}
+  };
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
 export default MealDetailScreen;
